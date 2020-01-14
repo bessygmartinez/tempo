45dab95a5d3a1188e31ffd296cef59a8db5b2921
RV <rgvegajr@gmail.com>
2020-01-13 20:56:12 -0500

    initial db schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,45 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS gitdown;
+CREATE DATABASE gitdown;
+USE gitdown;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE bandmbrs (
+	id Int( 11 ) AUTO_INCREMENT NOT NULL,
+	name VARCHAR( 50 ) NOT NULL,
+	instrument VARCHAR( 50 ),
+	hometown VARCHAR(100),
+    influence VARCHAR(100),
+    bio TEXT(1000),
+    profilepic VARCHAR(50),
+	/* Set ID as primary key */
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE discog (
+	id Int( 11 ) AUTO_INCREMENT NOT NULL,
+	title VARCHAR( 255) NOT NULL,
+	year VARCHAR( 255 ) NOT NULL,
+	tracks Int(11),
+	/* Set ID as primary key */
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE tour (
+	id Int( 11 ) AUTO_INCREMENT NOT NULL,
+    venue VARCHAR(255),
+	city VARCHAR( 255) NOT NULL,
+	state VARCHAR( 255 ) NOT NULL,
+	date DATE NOT NULL,
+    time TIME,
+	/* Set ID as primary key */
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE merch (
+	itemid Int( 11 ) AUTO_INCREMENT NOT NULL,
+    item_name VARCHAR(255),
+	type VARCHAR( 255) NOT NULL,
+	price VARCHAR( 255 ) NOT NULL,
+	inventory INT(200)NOT NULL,
+	/* Set ID as primary key */
+	PRIMARY KEY (itemid)
+);

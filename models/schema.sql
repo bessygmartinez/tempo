4c1ca635f7202fc3e2039840e31661ce912575c8
RV <rgvegajr@gmail.com>
2020-01-15 20:45:51 -0500

    new db schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS tempo;
+CREATE DATABASE tempo;
+USE tempo;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE bands (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	photoURL VARCHAR(255),
+	hometown VARCHAR(100),
+    genre VARCHAR(100),
+    bio TEXT(1000),
+	/* Set ID as primary key */
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE discog (
+	id INT AUTO_INCREMENT NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	year VARCHAR(255) NOT NULL,
+	tracks INT,
+	/* Set ID as primary key */
+	PRIMARY KEY (id)
+);

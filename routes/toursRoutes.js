48e91fcb8a46581b8cfd43f27ca7ffc2554b36ad
Bessy Gee <nakiwarai13@gmail.com>
2020-01-24 15:50:30 -0500

    created GET route to find all tours

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -1,1 +1,14 @@
 let db = require("../models");
+
+module.exports = function(app) {
+    //Get all tours
+    app.get("/api/tours", function(req, res) {
+        db.Tours.findAll({
+            include: [
+                { model: db.Band, attributes: ["bandName"]}
+            ]
+        }).then(function(dbBands) {
+            res.json(dbBands);
+        });
+    });
+}

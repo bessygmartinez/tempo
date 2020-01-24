403b86b6616a7d9d169a5f8fcea3666c2a4bc701
Bessy Gee <nakiwarai13@gmail.com>
2020-01-23 21:33:32 -0500

    added /api/bands to band routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,37 @@ let db = require("../models");
 
 module.exports = function(app) {
     // Get all bands
-    app.get("/bands", function(req, res) {
-        db.Band.findAll({}).then(function(dbBands) {
+    app.get("/api/bands", function(req, res) {
+        db.Band.findAll({
+            include: [
+                { model: db.Discog },
+                { model: db.Tours }
+            ]
+        }).then(function(dbBands) {
             res.json(dbBands);
         });
     });
 
     //Create a new band
-    app.post("/bands", function(req, res) {
-        // let newBand = new Band({
-        //     bandName: req.body.bandName,
-        //     bandPhotoURL: req.body.bandPhotoURL,
-        //     bandHometown: req.body.bandHometown,
-        //     bandGenre: req.body.bandGenre,
-        //     bandBio: req.body.bandBio
-        // });
-        // console.log("you hit the POST route!");
-        // console.log(newBand);
-        db.Band.create(req.body).then(function(dbBand) {
+    app.post("/api/bands", function(req, res) {
+        let newBand = new Band({
+            bandName: req.body.bandName,
+            bandPhotoURL: req.body.bandPhotoURL,
+            bandHometown: req.body.bandHometown,
+            bandGenre: req.body.bandGenre,
+            bandBio: req.body.bandBio
+        });
+        console.log("you hit the POST route!");
 
+        db.Band.create(newBand).then(function(dbBand) {
+
+            let newBandDiscog = new Discog({
+                discTitle: req.body.discTitle,
+                discYear: req.body.discYear,
+                discTracks: req.body.discTracks,
+                bandId: req.body.newBand.bandId
+        
+            })
 
             // db.Band.create(newBand).then(function(dbBand) {
             console.log(dbBand);

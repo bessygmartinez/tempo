e9c04c3322686141ba735bd8cb4a57d010299a4e
Bessy Gee <nakiwarai13@gmail.com>
2020-01-25 10:31:55 -0500

    added default values to discTitle/Year/Tracks and added bandId attribute

--- models/discog.js
+++ models/discog.js
@@ -6,9 +6,22 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.INTEGER,
             primaryKey: true
         },
-        discTitle: DataTypes.STRING,
-        discYear: DataTypes.STRING,
-        discTracks: DataTypes.STRING
+        discTitle: {
+            type: DataTypes.STRING,
+            defaultValue: "title"
+        },
+        discYear: {
+            type: DataTypes.STRING,
+            defaultValue: "year"
+        },
+        discTracks: {
+            type: DataTypes.STRING,
+            defaultValue: "tracks"
+        },
+        bandId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
         }, {
             freezeTableName: true
         });

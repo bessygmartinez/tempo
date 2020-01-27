1647a2e9d1f17fe2c7a3b72d847855b6ac09cb3c
Bessy Gee <nakiwarai13@gmail.com>
2020-01-26 19:34:02 -0500

    changed database names to plural

--- models/band.js
+++ models/band.js
@@ -1,7 +1,7 @@
 let db = require("../models");
 
 module.exports = function(sequelize, DataTypes) {
-    var Band = sequelize.define("Band", {
+    var Band = sequelize.define("Bands", {
         bandId: {
             type: DataTypes.INTEGER,
             primaryKey: true
@@ -10,13 +10,16 @@ module.exports = function(sequelize, DataTypes) {
         bandPhotoURL: DataTypes.STRING,
         bandHometown: DataTypes.STRING,
         bandGenre: DataTypes.STRING,
-        bandBio: DataTypes.TEXT
+        bandBio: DataTypes.TEXT,
     }, {
+        timestamps: false
+    },
+    {
         freezeTableName: true
     });
 
     Band.associate = (models) => {
-        Band.hasMany(models.Discog, { foreignKey: "bandId", onDelete: "CASCADE" });
+        Band.hasMany(models.Discogs, { foreignKey: "bandId", onDelete: "CASCADE" });
         Band.hasMany(models.Tours, { foreignKey: "bandId", onDelete: "CASCADE" });
     }
     return Band;

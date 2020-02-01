406b44766325d0d201bfd5efb714e3e864392eec
RV <rgvegajr@gmail.com>
2020-02-01 15:27:39 -0500

    corrected FE delete code to properly call BE delete route and functionality.  Unable to force redirect to index route following delete.  Unable to prevent page refresh resulting in 404 error due to using the now-deleted bandId

--- routes/bandRoutes.js
+++ routes/bandRoutes.js
@@ -2,46 +2,50 @@ let db = require("../models");
 
 module.exports = function(app) {
     //Get all bands
-    app.get("/api/bands", function(req, res) {
-        db.Bands.findAll({
-            include: [
-                { model: db.Discogs },
-                { model: db.Tours }
-            ]
-        }).then(function(dbBands) {
-            res.json(dbBands);
-        });
-    });
+    // app.get("/api/bands", function(req, res) {
+    //     db.Bands.findAll({
+    //         include: [
+    //             { model: db.Discogs },
+    //             { model: db.Tours }
+    //         ]
+    //     }).then(function(dbBands) {
+    //         res.json(dbBands);
+    //     });
+    // });
 
-    app.get("/api/bands/:bandId", function(req, res) {
-        db.Bands.findOne({
-            include: [
-                { model: db.Discogs },
-                { model: db.Tours }
-            ],
-            where: {
-               bandId: req.params.bandId
-            }
-        }).then(function(dbBand) {
-            res.json(dbBand);
-        });
-    });
+    // app.get("/api/bands/:bandId", function(req, res) {
+    //     db.Bands.findOne({
+    //         include: [
+    //             { model: db.Discogs },
+    //             { model: db.Tours }
+    //         ],
+    //         where: {
+    //            bandId: req.params.bandId
+    //         }
+    //     }).then(function(dbBand) {
+    //         res.json(dbBand);
+    //     });
+    // });
 
     //Create a new band
     app.post("/api/bands", function(req, res) {
-        console.log("You hit the band POST route!");
+        console.log("You hit the new band POST bandRoute!");
         db.Bands.create(req.body);
     });
 
     //Delete a band by id
     app.delete("/api/bands/:bandId", function(req, res) {
+        console.log("You hit the back end band delete bandRoute!");
+        console.log("band id:");
+        console.log(req.params.bandId);
         db.Bands.destroy({
-           where: { 
-               bandId: req.params.bandId
-            }
-        })
-        .then(function(dbBand) {
-            res.json(dbBand);
-        });
+                where: {
+                    bandId: req.params.bandId
+                }
+            })
+            .then(function(dbBands) {
+                console.log("FLASH DELETE SUCCESS");
+                res.json(dbBands);
+            });
     });
 };

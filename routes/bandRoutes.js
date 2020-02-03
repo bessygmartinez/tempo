f1362ffc9721d01fb1ad6efc59620d14e09fe66c
Bessy Gee <nakiwarai13@gmail.com>
2020-02-03 13:40:14 -0500

    uncommented GET routes

--- routes/bandRoutes.js
+++ routes/bandRoutes.js
@@ -2,30 +2,30 @@ let db = require("../models");
 
 module.exports = function(app) {
     //Get all bands
-    // app.get("/api/bands", function(req, res) {
-    //     db.Bands.findAll({
-    //         include: [
-    //             { model: db.Discogs },
-    //             { model: db.Tours }
-    //         ]
-    //     }).then(function(dbBands) {
-    //         res.json(dbBands);
-    //     });
-    // });
+    app.get("/api/bands", function(req, res) {
+        db.Bands.findAll({
+            include: [
+                { model: db.Discogs },
+                { model: db.Tours }
+            ]
+        }).then(function(dbBands) {
+            res.json(dbBands);
+        });
+    });
 
-    // app.get("/api/bands/:bandId", function(req, res) {
-    //     db.Bands.findOne({
-    //         include: [
-    //             { model: db.Discogs },
-    //             { model: db.Tours }
-    //         ],
-    //         where: {
-    //            bandId: req.params.bandId
-    //         }
-    //     }).then(function(dbBand) {
-    //         res.json(dbBand);
-    //     });
-    // });
+    app.get("/api/bands/:bandId", function(req, res) {
+        db.Bands.findOne({
+            include: [
+                { model: db.Discogs },
+                { model: db.Tours }
+            ],
+            where: {
+               bandId: req.params.bandId
+            }
+        }).then(function(dbBand) {
+            res.json(dbBand);
+        });
+    });
 
     //Create a new band
     app.post("/api/bands", function(req, res) {
